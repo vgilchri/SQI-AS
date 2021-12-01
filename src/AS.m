671936e0f98dae9f2636e035f914fb52af2e907b
vgilchri <82121507+vgilchri@users.noreply.github.com>
2021-12-01 13:35:21 -0500

    Update AS.m

--- src/AS.m
+++ src/AS.m
@@ -208,8 +208,8 @@ presign := function(sk,pk,K,phi_K,isom_K,J,phi_J,epsilon, E_Y, P_Y, Q_Y)
 		"chain done";
 		
 		"presign done";
-
-	return commit_time,challenge_time,klpt_time,translate_time,sign_time,verif_time,Valuation(Z!Norm(sign_ideal),2), tau_P, tau_Q, presign_isogeny,tau_deg;
+	// not return verif_time for now
+	return commit_time,challenge_time,klpt_time,translate_time,sign_time,Valuation(Z!Norm(sign_ideal),2), tau_P, tau_Q, presign_isogeny,tau_deg;
 end function;
 
 
@@ -288,7 +288,8 @@ Test_sqias:=procedure()
 			"about to gen witness";
 			wit, E_Y, P_Y, Q_Y:=sqias_witness_gen();
 			"witness gen done, starting key gen";
-			commit_time,challenge_time,klpt_time,translate_time,sign_time,verif_time,size,tau_P,tau_Q,presign_isogeny,tau_deg:=presign(sk,pk,K,phi_K,isom_K,J,phi_J,epsilon, E_Y,P_Y, Q_Y);
+			// not returning verif_time for now
+			commit_time,challenge_time,klpt_time,translate_time,sign_time,size,tau_P,tau_Q,presign_isogeny,tau_deg:=presign(sk,pk,K,phi_K,isom_K,J,phi_J,epsilon, E_Y,P_Y, Q_Y);
 			"key gen done, about to sign";
 			sig:=adapt(presign_isogeny,wit,P_Y, Q_Y, tau_P, tau_Q, tau_deg);
 			"sign done, about to extract";
