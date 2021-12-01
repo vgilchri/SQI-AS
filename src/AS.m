98c651d63ca96de79599e8115e2b9573e0d8a991
vgilchri <82121507+vgilchri@users.noreply.github.com>
2021-12-01 13:19:56 -0500

    Update AS.m

--- src/AS.m
+++ src/AS.m
@@ -199,9 +199,10 @@ presign := function(sk,pk,K,phi_K,isom_K,J,phi_J,epsilon, E_Y, P_Y, Q_Y)
 		counter:=1;
 		"starting chain of evals";
 		repeat
-		tau_P:=Evaluate(phi_K[counter], [tau_P])[1];
-		tau_Q:=Evaluate(phi_K[counter], [tau_Q])[1];
-	until counter = #phi_K ;
+			tau_P:=Evaluate(phi_K[counter], [tau_P])[1];
+			tau_Q:=Evaluate(phi_K[counter], [tau_Q])[1];
+			counter:=counter+1;
+		until counter = #phi_K +1;
 		"chain done";
 		tau_deg:= phi_K`degree;
 		"presign done";
