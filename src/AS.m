b29534c95b9254b6dd6b0cd301fbaa293f7368c2
vgilchri <82121507+vgilchri@users.noreply.github.com>
2022-02-06 14:21:43 -0500

    Update AS.m

--- src/AS.m
+++ src/AS.m
@@ -278,6 +278,7 @@ presign := function(sk,pk,K,phi_K,isom_K,J,phi_J,epsilon, E_Y, P_Y, Q_Y)
 		repeat
 			"counter is";
 			counter;
+			"deg is"+ phi_K[counter]`degree;
 			M:=Montgomery(tau_P`curve,Parent(tau_P`X)!1);
 			P_A := Lift(tau_P,M);
 			tau_P:=Evaluate(phi_K[counter], [tau_P])[1];
