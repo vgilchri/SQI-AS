cfa14ff0cfe4b44ff1d72014e3159306217d4425
vgilchri <82121507+vgilchri@users.noreply.github.com>
2022-02-10 18:04:46 -0500

    Update AS.m

--- src/AS.m
+++ src/AS.m
@@ -278,12 +278,8 @@ presign := function(sk,pk,K,phi_K,isom_K,J,phi_J,epsilon, E_Y, P_Y, Q_Y)
 		repeat
 			"counter is";
 			counter;
-			"deg is";
-			phi_K[counter]`degree;
-			"map is";
-			phi_K[counter];
 			M:=Montgomery(tau_P`curve,Parent(tau_P`X)!1);
-			P_A := Lift(tau_P,M);
+			P_A:=Lift(tau_P,M);
 			tau_P:=Evaluate(phi_K[counter], [tau_P])[1];
 			tau_Q:=Evaluate(phi_K[counter], [tau_Q])[1];
 			tau_deg*:=phi_K[counter]`degree;
